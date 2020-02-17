1306bcc3e6bd57717c6ad358c23ee82b0744db46
Maria Portela <56956042+sqblz@users.noreply.github.com>
2020-02-17 19:04:30 +0000

    recognize squares in the image

--- Image Analysis/main.m
+++ Image Analysis/main.m
@@ -1,10 +1,33 @@
-I = imread("images/mock1.jpg");
-imshow(I);
 
-G = rgb2gray(I);
-imshow(G);
+clear all;
+originalImage = imread("images/mock1.jpg");
+grayImage = rgb2gray(originalImage);
 
-E = edge(G, 'canny');
+binaryImage = imbinarize(grayImage);
+
+cc = bwconncomp(binaryImage);
+stats = regionprops(cc, grayImage, 'Area', 'BoundingBox');
+minArea = size(originalImage,1)*size(originalImage, 2)*0.001;
+idx = find([stats.Area] > minArea);
+labelImage = ismember(labelmatrix(cc), idx);
 
 figure;
-imshow(E);
+imshowpair(binaryImage, originalImage, 'montage');
+hold on;
+
+squares = [];
+
+for n = 1:size(stats,1)
+    box = stats(n).BoundingBox;
+    w = box(3);
+    h = box(4);
+    x = box(1);
+    y = box(2);
+    
+    if w*h > minArea && round(w/h) == 1
+        rectangle('Position', [x y w h], 'EdgeColor', 'r');
+        squares = [squares [x y w h]];
+    end
+end
+
+
