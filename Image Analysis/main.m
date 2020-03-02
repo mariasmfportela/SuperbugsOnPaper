364e08534ae37a6a33e7de037c478a314760e105
Maria <maria.m.portela@gmail.com>
2020-03-02 16:24:29 +0000

    Eliminated unused functions

--- Image Analysis/main.m
+++ Image Analysis/main.m
@@ -1,5 +1,4 @@
 %export the image and convert into a binary image
-figure;
 origImg = imread("images/mock2.jpg");
 
 %convert image in binary matrix
@@ -35,29 +34,3 @@ plot(center(1), center(2), 'g*');
 x = (origin(1)-100000):(origin(1)+10000);
 y = tand(theta)*x + origin(2) - tand(theta)*origin(1);
 plot(x,y,'b');
-
-% %get the center of the larger squares in the pattern, and the center of the
-% %smaller square
-% lg_center = get_center(sqrs);
-% 
-% %get the direction from the larger center to the test lines
-% %angle with the coordinate system as a value between -180 and 180 degrees
-% theta = get_angle(lg_center, sm_center);
-% 
-% %get unit length, defined as side of smaller square
-% unit = get_unit(sqrs(4).BoundingBox, theta, lg_center);
-% 
-% %find if control line is present
-% ctrl_line(origImg, lg_center, theta, unit);
-% 
-% 
-% for n = 1:length(sqrs)
-%     rectangle('Position', sqrs(n).BoundingBox, 'EdgeColor', 'r');
-% end
-% 
-% plot(lg_center(1), lg_center(2), 'g*');
-% plot(sm_center(1), sm_center(2), 'g*');
-% 
-% x = (lg_center(1)-100000):(lg_center(1)+10000);
-% y = tand(theta)*x + lg_center(2)-tand(theta)*lg_center(1);
-% plot(x,y,'b');
