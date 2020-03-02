582cfb0d05c68049c0609e25da60e8a3a2ee4edb
Maria <maria.m.portela@gmail.com>
2020-03-02 19:08:25 +0000

    Mask image to get control line

--- Image Analysis/ctrl_line.m
+++ Image Analysis/ctrl_line.m
@@ -1,26 +1,33 @@
 function output = ctrl_line(img, origin, angle, u)
 %coordinates of the corners of the control line when angle = 0, relative to
 %the origin
-p1 = [3*u; 2.5*u];
-p2 = [3*u; -2.5*u];
-p3 = [3.9*u; 2.5*u];
-p4 = [3.9*u; -2.5*u];
-corners = [p1 p2 p3 p4];
+vertices = [3*u 3*u 3.8*u 3.8*u; -2.4*u 2.4*u 2.5*u -2.5*u];
 
-%rotation by angle
-corners = [cosd(angle) -sind(angle); sind(angle) cosd(angle)]*corners;
+%rotate
+vertices = [cosd(angle) -sind(angle); sind(angle) cosd(angle)]*vertices;
 
-%translation to center at origin
-corners = corners + [origin.' origin.' origin.' origin.'];
+%translation
+vertices = vertices + [origin(1)*ones(1, 4); origin(2)*ones(1,4)];
 
-%plot to check
 figure;
 imshow(img);
 hold on;
+plot(vertices(1,:), vertices(2,:), 'b*');
 
-for n = 1:4
-    plot(corners(1,n), corners(2,n), 'b*');
-end
+x = double(vertices(1,:));
+y = double(vertices(2,:));
+
+m = size(img,1);
+n = size(img,2);
+BW = poly2mask(x,y,m,n);
+
+img(repmat(BW,[1,1,3])==0) = 0;
+
+%plot to check
+
+imshow(img);
+
+
 
 end
 
