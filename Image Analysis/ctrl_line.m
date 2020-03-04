34254a41ca445ef7d6a8c815fbac9a4df62a9bc1
Maria <maria.m.portela@gmail.com>
2020-03-04 10:39:30 +0000

    Calculate mean for green channel

--- Image Analysis/ctrl_line.m
+++ Image Analysis/ctrl_line.m
@@ -9,25 +9,26 @@ vertices = [cosd(angle) -sind(angle); sind(angle) cosd(angle)]*vertices;
 %translation
 vertices = vertices + [origin(1)*ones(1, 4); origin(2)*ones(1,4)];
 
-figure;
-imshow(img);
-hold on;
-plot(vertices(1,:), vertices(2,:), 'b*');
-
+%create mask for the region of interest
 x = double(vertices(1,:));
 y = double(vertices(2,:));
-
 m = size(img,1);
 n = size(img,2);
 BW = poly2mask(x,y,m,n);
 
+%apply mask to image
 img(repmat(BW,[1,1,3])==0) = 0;
 
-%plot to check
+%extract green channel of masked image
+greenChannel = img(:,:,2);
 
-imshow(img);
+%process data
+greenChannel = reshape(greenChannel, 1, []);
+greenChannel(greenChannel == 0) = [];
+greenChannel = sort(greenChannel);
+greenChannel = greenChannel(floor(0.1*length(greenChannel)):ceil(0.9*length(greenChannel)));
 
-
+meanGreen = mean(greenChannel);
 
 end
 
