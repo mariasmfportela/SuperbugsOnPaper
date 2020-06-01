74a53f902f125ef2cd53f232c8596d08d1439b10
Maria <maria.m.portela@gmail.com>
2020-06-01 17:45:40 +0100

    Added flask

--- Website/main.py
+++ Website/main.py
@@ -4,12 +4,17 @@ Created on Tue Mar 10 14:19:03 2020
 
 @author: maria
 """
-
+import os
 import resultanalysis
 from flask import Flask, render_template, request
+#commented out bits for file upload - work in progress
+#from werkzeug.utils import secure_filename
+
+#UPLOAD_FOLDER = '/user_files'
+#ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-r = resultanalysis.initialize()
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
 def form():
@@ -18,8 +23,8 @@ def form():
 
 @app.route('/results', methods=['GET', 'POST'])
 def analyse():
-    img = request.form["user_result"]
-    return render_template("results.html", result="img")
+    #img = request.form["user_result"]
+    return render_template("results.html", result="result")
 
 
 if __name__ == "__main__":
